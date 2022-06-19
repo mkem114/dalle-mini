17f0b0b47ff004d9fe133a1192d2f99267972606
mkem114 <20897849+mkem114@users.noreply.github.com>
2022-06-19 19:14:18 +1000

    Convert to WSGI server

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to run the docker image. Change or remove GPU flag if you dont have nvidia-docker or the needed GPUs
-docker run --rm -it -p 8888:8888  --gpus all  -v "${PWD}":/workspace dalle-mini:latest
+docker run -e WANDB_API_KEY=$WANDB_API_KEY --rm -it -p 8000:8000 --gpus 1 -v "${PWD}":/workspace dalle-mini:latest
